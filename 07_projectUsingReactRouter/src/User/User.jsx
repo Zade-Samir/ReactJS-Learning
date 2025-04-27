635355e47501246350e9873a9af153ab26ceefa1
Samir-Zade <samirzadeofficial@gmail.com>
2025-04-27 17:51:59 +0530

    update in User

--- 07_projectUsingReactRouter/src/User/User.jsx
+++ 07_projectUsingReactRouter/src/User/User.jsx
@@ -5,7 +5,7 @@ function User() {
     const {userid} = useParams()
 
     return (
-        <div className="bg-gray-400 text-white text-3xl p-4">User: {userid}</div>
+        <div className="bg-gray-400 text-white text-3xl p-4">User Info: {userid}</div>
     )
 }
 
